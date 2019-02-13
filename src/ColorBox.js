4c19d3060691b86783d65a52a154964708fdb287
Sammy Wu <swucode@gmail.com>
2019-02-13 20:01:46 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,5 +9,9 @@ export default class ColorBox extends Component {
         {opacity >= 0.2 ? <ColorBox opacity={reduceOpacity} /> : null}
       </div>
     );
+<<<<<<< HEAD
   }
 }
+=======
+  }
+>>>>>>> d7eeac9b081145a5b9f2a4a68aea6b96e1b1a637
